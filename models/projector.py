ab2c18becf60f5d1e86eaea1c23f2c735780b7e6
Thanhoanh <152255716+Thanhoanh@users.noreply.github.com>
2025-04-22 18:44:31 +0700

    Update projector.py

--- models/projector.py
+++ models/projector.py
@@ -1,8 +1,5 @@
-
-import torch.nn as nn
-
 class ProjectorMLP(nn.Module):
-    def __init__(self, input_dim=1024, output_dim=4096):
+    def __init__(self, input_dim=512, output_dim=4096):  # CHUYỂN 1024 → 512
         super().__init__()
         self.mlp = nn.Sequential(
             nn.Linear(input_dim, output_dim),
